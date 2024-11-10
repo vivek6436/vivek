43122713a767a858005044c6ce0b2883ab1ec488
vivek6436 <vivekrock1090@gmail.com>
2024-11-10 15:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
